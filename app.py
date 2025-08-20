4c267db062cb242a8de9b96db3fdf29e4207962b
monichbaba <kumarajai523@gmail.comcd Desktop/flask-test-portal>
2025-08-20 23:08:26 +0530

    Updated in app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import json
 
 app = Flask(__name__)
 
-PASSWORD = "jaikanhiyalal"
+PASSWORD = "jaimahakal"
 
 with open('mcqs/questions.json', 'r', encoding='utf-8') as f:
     questions = json.load(f)
